025c4ebf4c40f62c3bcc49d0bf5a8a6347e640ee
elephant_seal <mikan.ha.orange123@gmail.com>
2023-03-01 08:06:00 +0900

    時刻を追加

--- src/main/java/com/example/helloworld/Helloworld.java
+++ src/main/java/com/example/helloworld/Helloworld.java
@@ -2,12 +2,14 @@ package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @RestController
 public class Helloworld {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        Date date = new Date();
+        return "hello world" + date;
     }
 }
